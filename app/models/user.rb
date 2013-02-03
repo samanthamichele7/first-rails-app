cb5c79725fb157400dc95808f265d24e9b3a997e
Samantha Geitz <samanthamichele7@gmail.com>
2013-02-03 09:13:58 -0600

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -24,8 +25,14 @@ class User < ActiveRecord::Base
   					format: { with: VALID_EMAIL_REGEX },
   					uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
   validates :password_confirmation, presence: true
 
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,14 +4,14 @@ describe "UserPages" do
   
   subject { page }
 
-  describe "signup page" do
-  	before {visit signup_path}
+    describe "signup page" do
+    	before {visit signup_path}
 
-  	it { should have_selector('h1', 	text: 'Sign up') }
-  	it { should have_selector('title', 	text: full_title('Sign up')) }
-  end
+    	it { should have_selector('h1', 	text: 'Sign up') }
+    	it { should have_selector('title', 	text: full_title('Sign up')) }
+    end
 
-  	describe "profile page" do
+    describe "profile page" do
 	  	let(:user) { FactoryGirl.create(:user) }
 	  	before { visit user_path(user) }
 
@@ -41,6 +41,16 @@ describe "UserPages" do
 
         it "should create a user" do
           expect { click_button submit }.to change(User, :count).by(1)
+        end
+      
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          it { should have_link('Sign out') }
         end
       end
     end
